866c3f010dc28ccab4815dd4a010f241b9c1e15a
Stacey <sachungo1@gmail.com>
2019-01-28 17:16:32 +0300

    WIP: Setup webpack

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,21 +9,13 @@ addDecorator(checkA11y);
  * Read more:
  *   https://webpack.js.org/guides/dependency-management/#require-context
  *   https://storybook.js.org/basics/writing-stories/#loading-stories-dynamically
- * E.g.
- * const requires = require.context('../src', true, '/\.stories\.js$/');
- * const loadStories = () => {
- *   requires.key().foreach(filename => requires(filename));
- * }
- *
- * Note that the components will be in their own folders as:
- *
- * /Component folder
- *   /myfile.js
- *   __stories__
- *     /myfile.stories.js
- *   __tests__
- *     /myfile.test.js
  */
+
+// const required = require.context('../src', true, '/\.stories\.js$/');
+// const loadStories = () => {
+//   required.key().foreach(filename => required(filename));
+// }
+
 const loadStories = () => {
   require('../src/Example/button.stories.js');
 };
