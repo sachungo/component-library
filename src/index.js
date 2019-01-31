9a5ccdfdff31bb1b69f97a1897b9b3849a49dbe9
Stacey <sachungo1@gmail.com>
2019-01-31 11:37:36 +0300

    Webpack: testing source map with intentional error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import printMe from './print';
+
 function testing() {
   const element = document.createElement('div');
   element.innerHTML = "Testing Webpack setup";
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console! ;)';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
   return element;
 }
 
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function PrintMe() {
+  consnoel.log('I get called from print.js!');
+}
