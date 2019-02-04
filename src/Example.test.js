b48cfdae5e63e3ab712b250ee8d9b30d2c7bbf41
Stacey <sachungo1@gmail.com>
2019-02-04 15:46:52 +0300

    Added the debug option for tests

--- src/Example.test.js
+++ src/Example.test.js
@@ -4,4 +4,9 @@ describe('Example test', () => {
   it('renders without exceptions', () => {
     expect(() => shallow(<Example />)).not.toThrow();
   });
+
+  it('renders the text', () => {
+    const wrapper = shallow(<Example />);
+    expect(wrapper).toHaveText('Testing test setup');
+  });
 });
