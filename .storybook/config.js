84f392de468f93143a685b995384a41ad1caf367
Stacey <sachungo1@gmail.com>
2019-02-05 11:19:36 +0300

    Use require.context to dynamically import stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,23 +1,19 @@
-import '@storybook/addon-console';
-import { configure, addDecorator } from '@storybook/react';
-import { checkA11y } from '@storybook/addon-a11y';
+import "@storybook/addon-console";
+import { configure, addDecorator } from "@storybook/react";
+import { checkA11y } from "@storybook/addon-a11y";
 
 addDecorator(checkA11y);
 
 /**
- * TODO: Dynamically load the stories using 'require.context` of webpack
+ * Dynamically load the stories using 'require.context` of webpack
  * Read more:
  *   https://webpack.js.org/guides/dependency-management/#require-context
  *   https://storybook.js.org/basics/writing-stories/#loading-stories-dynamically
  */
+const req = require.context("../src", true, /\.stories\.js$/);
 
-// const required = require.context('../src', true, '/\.stories\.js$/');
-// const loadStories = () => {
-//   required.key().foreach(filename => required(filename));
-// }
-
-const loadStories = () => {
-  require('../src/Example/button.stories.js');
-};
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
 
 configure(loadStories, module);
